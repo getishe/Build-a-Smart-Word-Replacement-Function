6a01c7080fb392bbbebe8a53dfde9baed3781adf
get-code-gif <getalemayhu@gmail.com>
2026-09-25 14:16:24 +0300

    Implement myReplace function for word replacement
    Add myReplace function to replace words with case sensitivity.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function myReplace(sentence, str, replaceWord){
+  let cap;
+  let replacewordCapital;
+  let regex = /[A-Z]/
+ 
+  let result = "";
+ 
+  if(!sentence.includes(str.trim())){ 
+   return "String not found";
+}
+  
+cap = str[0];   
+if(regex.test(cap)){
+  replacewordCapital = replaceWord[0].toUpperCase() + replaceWord.slice(1);
 
+}
+else {
+  replacewordCapital = replaceWord[0].toLowerCase() + replaceWord.slice(1);
+}
+  result = sentence.replace(str, replacewordCapital); 
+
+  return result;
+}
+
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
